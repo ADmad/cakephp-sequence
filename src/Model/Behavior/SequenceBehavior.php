aba4074af5296617350f252a677f925e0a83767e
Walther Lalk <83255+dakota@users.noreply.github.com>
2018-03-20 09:24:17 +0200

    Select only the order field
    Small optimisation

--- src/Model/Behavior/SequenceBehavior.php
+++ src/Model/Behavior/SequenceBehavior.php
@@ -477,6 +477,7 @@ class SequenceBehavior extends Behavior
 
         // Find the last record in the set
         $last = $this->_table->find()
+            ->select([$orderField])
             ->where($scope)
             ->order([$orderField => 'DESC'])
             ->limit(1)
